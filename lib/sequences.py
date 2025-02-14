da9188636cc314d609ab7919c6c51ea9db3cb98f
mgenge254 <jeremiah.orodi@student.moringaschool.com>
2025-02-14 19:05:34 +0300

    new commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])  # Print an empty list when n == 0
+        return
+    
+    fibonacci_sequence = [0, 1]
+    
+    for _ in range(2, n):
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+    print(fibonacci_sequence[:n])
